b797a497e456eec905fc83e6cc0c2be5305d97d0
OhhHarrods <42657619+OhhHarrods@users.noreply.github.com>
2021-11-14 18:16:50 -0400

    audio fix

--- bundle.js
+++ bundle.js
@@ -10158,7 +10158,7 @@ function photoChange() {
 }
 
 function playAudio() {
-  audio = new Audio('/audio/KSLV - Dynamic.mp3');
+  audio = new Audio('KSLV - Dynamic.mp3');
   audio.volume = 0.1;
 
   if (typeof audio.loop == 'boolean') {
